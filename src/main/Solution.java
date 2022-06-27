915a523ca0b13792d9b6aa113741490519a4b553
wzh <337245354@qq.com>
2022-06-27 19:37:36 +0800

    1

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,14 +1,14 @@
+import java.util.*;
+
 class Solution {
 
 }
 
 
-//
+
 //public class Main {
-//
 //    public static void main(String[] args) {
-//        Main ma = new Main();
-//        System.out.println(ma.sayHello());
+//        Solution sl = new Solution();
+//        System.out.println(sl.sayHello());
 //    }
-//
 //}
