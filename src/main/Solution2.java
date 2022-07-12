b270d8e844e5f386e709ec7fa0069c1b4e4e51c0
wzh <337245354@qq.com>
2022-07-12 11:11:41 +0800

    1

---
+++ src/main/Solution2.java
@@ -0,0 +1,12 @@
+import DataStructure.TreeNode;
+
+import java.util.*;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.text.*;
+import java.util.concurrent.*;
+
+class Solution2 {
+
+}
+
